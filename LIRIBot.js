727a2ac58bad4f54df149f1fc1c910c122dd381d
chrisbrady510 <chrismbrady@gmail.com>
2019-07-28 13:38:53 -0700

    finished product

--- LIRIBot.js
+++ LIRIBot.js
@@ -1,28 +1,50 @@
 require("dotenv").config()
-var inquirer = require ("inquirer")
-var keys = require ("./keys")
+var inquirer = require("inquirer")
+var keys = require("./keys")
 var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify)
 
+function searchSpotify() {
+    inquirer.prompt([{
+        name: "songSearch",
+        message: "Enter which artist you'd like to search"
+    }]).then(inquireResponse => {
+        console.log(inquireResponse.songSearch)
 
+        spotify.search(
+            {
+                type: 'track',
+                query: inquireResponse.songSearch
+            },
+            function (err, res) {
+                if (err) throw err
+                console.log(res.tracks)
+            }
+        )
+        showOptions()
+    })
+}
 
-inquirer.prompt([{
-    type: "list",
-    name: "options",
-    message: "chooseoption",
-    choices: ["searchsongs byartist", "searchbands intown", "searchOMDB", "quit"]
+function showOptions() {
+    inquirer.prompt([{
+        type: "list",
+        name: "options",
+        message: "chooseoption",
+        choices: ["search songs by artist", "searchbands intown", "searchOMDB", "quit"]
 
-}]).then(inquireresponse=>{
-    
-    inquireresponse.options === "quit"
-    ? process.exit() 
-    : inquireresponse.options === "searchsongs byartist"
-    ? console.log(inquireresponse.options)
-    : inquireresponse.options === "searchbands intown"
-    ? console.log(inquireresponse.options)
-    : inquireresponse.options === "searchOMDB"
-    ? console.log(inquireresponse.options)
-    : console.log('unrecognized command')
-})
+    }]).then(inquireresponse => {
 
+        inquireresponse.options === "quit"
+            ? process.exit()
+            : inquireresponse.options === "search songs by artist"
+                ? searchSpotify()
+                : inquireresponse.options === "searchbands intown"
+                    ? console.log(inquireresponse.options)
+                    : inquireresponse.options === "searchOMDB"
+                        ? console.log(inquireresponse.options)
+                        : console.log('unrecognized command')
+    })
+}
+
+showOptions()
 
